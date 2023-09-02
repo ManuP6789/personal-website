ce5a8b1c1cf29c102040ffc13092affba951a280
ManuP6789 <143857167+ManuP6789@users.noreply.github.com>
2023-09-02 14:39:49 -0600

    Add files via upload

---
+++ header/header.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const video = document.getElementById("myVideo");
+    const videoPause = document.querySelector(".video-pause-button");
+    const pauseImage = document.querySelector(".pause-image");
+    const playImage = document.querySelector(".play-image");
+
+
+    videoPause.addEventListener("click", function(){
+        if (video.paused) {
+            video.play();
+            pauseImage.classList.remove('image-hide');
+            playImage.classList.add('image-hide');
+        } else {
+            video.pause();
+            pauseImage.classList.add('image-hide');
+            playImage.classList.remove('image-hide');
+        }
+    })
+})

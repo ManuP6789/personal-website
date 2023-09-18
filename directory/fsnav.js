267879d36607e15deee31570dbc019189b6e56f0
ManuP6789 <143857167+ManuP6789@users.noreply.github.com>
2023-09-17 21:21:29 -0600

    Update fsnav.js

--- directory/fsnav.js
+++ directory/fsnav.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navCloseElement = document.querySelector(".fsnav-close")
     const navOpenElement = document.querySelector(".menu-button")
+    const bodyElement = document.querySelector(".body")
     const fsNav = document.querySelector(".fsnav");
 
     navCloseElement.addEventListener("click", function(){
         fsNav.classList.remove('fsnav--open');
+        bodyElement.classList.remove('stop-scrolling')
     })
 
     navOpenElement.addEventListener("click", function(){
         fsNav.classList.add('fsnav--open');
+        bodyElement.classList.add('stop-scrolling')
     })
 })

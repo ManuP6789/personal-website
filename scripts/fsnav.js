3285b2b51cb8e8d7f20406481ef3c97d255ebd81
ManuP6789 <manuel.pena654576@tufts.edu>
2023-11-23 14:12:21 -0800

    Added anchor links

--- scripts/fsnav.js
+++ scripts/fsnav.js
@@ -13,4 +13,25 @@ document.addEventListener("DOMContentLoaded", function() {
         fsNav.classList.add('fsnav--open');
         bodyElement.classList.add('stop-scrolling')
     })
+
+    // Event listeners for anchor links in the navigation
+    fsNav.querySelectorAll('a.fsnav-link[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      // Close the navigation
+      fsNav.classList.remove('fsnav--open');
+      bodyElement.classList.remove('stop-scrolling');
+
+    //   // Scroll to the target section
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth'
+        })
+      }
+    })
+  })
 })

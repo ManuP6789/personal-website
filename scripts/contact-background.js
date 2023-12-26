39975cf44e7862d37ccb803abf39003a4e7f4cee
ManuP6789 <manuel.pena654576@tufts.edu>
2023-12-25 21:32:15 -0800

    Contacts done
    Finished contacts editied some other stuff

---
+++ scripts/contact-background.js
@@ -0,0 +1,37 @@
+const contactAnimation = document.getElementById
+('contactBackground');
+
+const numOfColorBoxes = 400;
+
+for (let i = 0; i < numOfColorBoxes; i++) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    contactBackground.append(colorBox);
+
+    // Add event listeners for mouseover and mouseout
+    colorBox.addEventListener('mouseover', handleMouseOver);
+}
+
+function handleMouseOver() {
+    const randomIndex = Math.floor(Math.random() * numOfColorBoxes);
+    const randomColorBox = contactAnimation.children[randomIndex];
+
+    // Change the style of the randomly selected box
+    randomColorBox.style.backgroundColor = getRandomColor();
+
+    // Reset the color to black after 1 seconds using setTimeout
+    setTimeout(function() {
+        randomColorBox.style.backgroundColor = '#0a0a0a';
+    }, 1000); 
+}
+
+
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#87cefa';
+    // for (let i = 0; i < 6; i++) {
+    //     color += letters[Math.floor(Math.random() * 16)];
+    // }
+    return color;
+}
---
+++ scripts/email.js
@@ -0,0 +1,20 @@
+// Function to open the default email client when the image is clicked
+function sendEmail() {
+    const emailAddress = 'manuel.pena.mt@gmail.com'; // Replace with your actual email address
+    const subject = 'Hello!'; // You can customize the email subject
+
+    const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}`;
+    window.location.href = mailtoLink;
+}
+
+// Attach the click event to the email image
+const emailImage = document.getElementById('email-image');
+emailImage.addEventListener('click', sendEmail);
+
+
+document.getElementById('linkedIn-image').addEventListener('click', function() {
+    // Specify the URL you want the image to link to
+    window.location.href = 'https://www.linkedin.com/in/manuel-pena-417262290/';})
+
+document.getElementById('GitHub-image').addEventListener('click', function() {
+    window.location.href = 'https://github.com/ManuP6789';})

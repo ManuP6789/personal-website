3d9d0c023e1b86b403d94742bcbbf66a4c8bd1cc
ManuP6789 <manuel.pena654576@tufts.edu>
2024-01-06 16:01:30 -0800

    Improved academics section
    added images and carousel

---
+++ scripts/slick-carousel.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOMContentLoaded event fired");
+
+    document.querySelectorAll("#ec-carousel").forEach(carousel => {
+        console.log("DOMContentLoaded event fired");
+        const items = carousel.querySelectorAll(".ec-carousel-item")
+        const buttonsHtml = Array.from(items, () => {
+            return '<span class="carousel-button"></span>';
+        })
+
+        carousel.insertAdjacentHTML("beforeend", `
+            <div class="carousel-nav">
+                ${buttonsHtml.join("")}
+            </div>
+        `);
+
+        const buttons = carousel.querySelectorAll(".carousel-button");
+
+        buttons.forEach((button, i) => {
+            button.addEventListener("click", () => {
+                // unselect items
+                items.forEach(item => item.classList.remove("ec-carousel-item-selected"));
+                buttons.forEach(button => button.classList.remove("ec-carousel-button-selected"));
+
+                items[i].classList.add("ec-carousel-item-selected");
+                button.classList.add("ec-carousel-button-selected");
+            });
+        });
+
+        // Select the first item on page load
+        items[0].classList.add("ec-carousel-item-selected");
+        buttons[0].classList.add("ec-carousel-button-selected");
+        console.log(buttonsHtml)
+    });
+})

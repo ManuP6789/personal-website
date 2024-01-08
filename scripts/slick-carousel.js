1ebff81d36f3cf2476d25b7113d01dbdae8f1561
ManuP6789 <manuel.pena654576@tufts.edu>
2024-01-07 19:48:05 -0800

    Academics coursework and more
    Added coursework section
    Added animation to academics
    Made academics responsive
    Removed html5 video controls

--- scripts/slick-carousel.js
+++ scripts/slick-carousel.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("DOMContentLoaded event fired");
 
     document.querySelectorAll("#ec-carousel").forEach(carousel => {

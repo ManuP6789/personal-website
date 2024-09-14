acf27d9360191cab3d858141c70ff2158ea1506f
ManuP6789 <manuel.pena654576@tufts.edu>
2024-09-14 11:05:14 -0400

    fix loading issue

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -2,17 +2,22 @@
  * @Author: Manuel Pena
  * @Date:   2024-09-14 10:35:30
  * @Last Modified by:   Your name
- * @Last Modified time: 2024-09-14 11:00:13
+ * @Last Modified time: 2024-09-14 11:04:52
  */
 document.addEventListener("DOMContentLoaded", function () {
   const preloader = document.getElementById("preloader");
   const video = document.getElementById("myVideo");
 
   const videoPromise = new Promise((resolve) => {
-    video.onloadeddata = resolve;
+    if (video.readyState >= 3) {
+      // 3 is HAVE_FUTURE_DATA, meaning data is available
+      resolve();
+    } else {
+      video.onloadeddata = resolve;
+    }
   });
 
-  Promise.all([imagePromise, videoPromise]).then(() => {
+  Promise.all([videoPromise]).then(() => {
     preloader.classList.add("hidden"); // Hide preloader
   });
 });

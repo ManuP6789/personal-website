ba8b9e82123b1562bb9ae1c0970eed8ef971270b
ManuP6789 <manuel.pena654576@tufts.edu>
2024-09-14 10:42:43 -0400

    Add loading screen

---
+++ scripts/preloader.js
@@ -0,0 +1,19 @@
+/**
+ * @Author: Manuel Pena
+ * @Date:   2024-09-14 10:35:30
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2024-09-14 10:42:11
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  const preloader = document.getElementById("preloader");
+  const video = document.getElementById("myVideo");
+
+  const videoPromise = new Promise((resolve) => {
+    video.onloadeddata = resolve;
+  });
+
+  Promise.all([imagePromise, videoPromise]).then(() => {
+    //   preloader.classList.add("visible"); // Hide preloader
+    console.log("wow");
+  });
+});

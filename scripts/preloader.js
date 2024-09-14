280d3ee0005196a68f2ca7c359de83e04e17d19d
ManuP6789 <manuel.pena654576@tufts.edu>
2024-09-14 10:59:07 -0400

    fix preloader

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -2,7 +2,7 @@
  * @Author: Manuel Pena
  * @Date:   2024-09-14 10:35:30
  * @Last Modified by:   Your name
- * @Last Modified time: 2024-09-14 10:42:11
+ * @Last Modified time: 2024-09-14 10:58:59
  */
 document.addEventListener("DOMContentLoaded", function () {
   const preloader = document.getElementById("preloader");
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   Promise.all([imagePromise, videoPromise]).then(() => {
-    //   preloader.classList.add("visible"); // Hide preloader
-    console.log("wow");
+    preloader.classList.add("visible"); // Hide preloader
   });
 });
